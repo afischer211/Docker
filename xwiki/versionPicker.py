9e95cb48fad8d2d1c06875402f627023136d7e96
Alexander Fischer <fischer.alexander@web.de>
2017-11-07 18:20:48 +0100

    added image-generation for xwiki

---
+++ xwiki/versionPicker.py
@@ -0,0 +1,12 @@
+import os
+VersionFile = open("xwikiVersion.html","r")
+
+
+latest = ""
+for line in VersionFile:
+    if line.find("<span class=\"download-version fill-LTS\">") != -1:
+        latest = line[line.index("<span class=\"download-version fill-LTS\">") + len("<span class=\"download-version fill-LTS\">"):line.index("</span>",line.index("<span class=\"download-version fill-LTS\">"))]
+
+print 'wget -q "http://download.forge.ow2.org/xwiki/xwiki-enterprise-web-' + latest + '.war" -P /'
+print 'unzip -q "/xwiki-enterprise-web-' + latest + '.war" -d /var/lib/tomcat7/webapps/xwiki'
+print 'rm -f "/xwiki-enterprise-web-' + latest + '.war"'
